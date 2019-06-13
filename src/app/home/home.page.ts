72ddf5404c40f690b50f91d2e33eaa77a8981d6b
Ivan Diaz <diazdiazivan@gmail.com>
2019-06-13 22:27:20 +0200

    demo inbrowserapp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( private iab: InAppBrowser, private platform: Platform ) {
+    this.platform.ready().then(()=> {
+      this.iab.create('https://www.varma.com/es/', '_system');
+    })
+  }
+
+  openblank(){
+    this.iab.create('https://www.varma.com/es/', '_blank');
+  }
+
+  openSystem(){
+    this.iab.create('https://www.varma.com/es/', '_system');
+  }
 
 }
